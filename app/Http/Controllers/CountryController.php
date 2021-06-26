dbed202c504ab8af4f6f7d8f4407da171a3d2737
oussama <osm2014@hotmail.fr>
2021-06-26 14:15:16 -0700

    added create and search functions to country model

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -4,12 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Models\Country;
 use Illuminate\Http\Request;
+use App\Http\Requests\CountryStoreRequest;
 
 class CountryController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
         $countries = Country::all();
+
+        if($request->has('search')) {
+            $countries = Country::where('name', 'like', "%{$request->search}%")->orWhere('country_code', 'like', "%{$request->search}%")->get();
+        }
         
         return view('countries.index', compact('countries'));
+    }
+
+    public function create() {
+
+        return view('countries.create');
+    }
+
+    public function store(CountryStoreRequest $request, Country $country) {
+        
+        Country::create($request->validated());
+
+        return  redirect()->route('countries.index')->with('message', 'Country saved ');
+    }
+
+    public function edit() {
+        //
     }
 }
